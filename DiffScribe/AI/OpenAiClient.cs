96b6e7a3aedf742af2980ceff8c9c812119421f5
sudosys <ergin.ahmedtaha@gmail.com>
2025-04-12 15:32:37 +0300

    refactor(OpenAiClient): refactor message sending and configuration handling

--- DiffScribe/AI/OpenAiClient.cs
+++ DiffScribe/AI/OpenAiClient.cs
@@ -5,8 +5,6 @@ namespace DiffScribe.AI;
 
 public class OpenAiClient(ConfigHandler configHandler)
 {
-    private readonly ToolConfiguration _config = configHandler.GetConfigurationFromFile();
-    
     public bool TestApiKeyValidity(string? apiKey = null)
     {
         try
@@ -21,12 +19,19 @@ public class OpenAiClient(ConfigHandler configHandler)
         return true;
     }
 
-    private ChatClient GetClient(string? apiKey = null) => new(model: _config.Llm, apiKey: apiKey ?? _config.ApiKey);
- 
-    public string SendMessage(ChatClient client, ChatMessage message)
+    public string SendMessage(params ChatMessage[] message)
+    {
+        var client = GetClient(apiKey: null);
+        return SendMessage(client, message);
+    }
+    
+    private string SendMessage(ChatClient client, params ChatMessage[] message)
     {
         var completion = client.CompleteChat(message);
 
         return completion.Value.Content[0].Text;
     }
+
+    private ChatClient GetClient(string? apiKey = null) => 
+        new(model: configHandler.Configuration.Llm, apiKey: apiKey ?? configHandler.Configuration.ApiKey);
 }
