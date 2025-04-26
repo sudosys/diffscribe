ac98a57787dcd7ddf8d9390b0fb9e6bdfeb2e9bb
sudosys <ergin.ahmedtaha@gmail.com>
2025-04-26 18:31:07 +0300

    fix(install.sh): use printf to append newline when updating PATH in profile

--- DiffScribe/Scripts/install.sh
+++ DiffScribe/Scripts/install.sh
@@ -16,6 +16,6 @@ sudo cp -r "$SRC"/* "$DEST/"
 
 PROFILE="$HOME/.profile" # update the profile file name depending on your shell
 if ! grep -qs "$DEST" "$PROFILE"; then
-    echo "export PATH=\"\$PATH:$DEST\"" >> "$PROFILE"
+    printf "\nexport PATH=\"\$PATH:$DEST\"" >> "$PROFILE"
     echo "Added $DEST to PATH. Restart your session."
 fi
