6a9153cff07cc78ef15a24cf88424d31a0984500
sudosys <ergin.ahmedtaha@gmail.com>
2025-04-26 22:51:13 +0300

    refactor(app): remove app configuration and related dependencies

--- DiffScribe/Program.cs
+++ DiffScribe/Program.cs
@@ -41,8 +41,7 @@ class Program
             .AddSingleton<GitRunner>()
             .AddScoped<OpenAiClient>()
             .AddSingleton<CommitGenerator>()
-            .AddSingleton<EncryptionService>()
-            .AddSingleton<AppConfiguration>();
+            .AddSingleton<EncryptionService>();
         
         return serviceCollection;
     }
