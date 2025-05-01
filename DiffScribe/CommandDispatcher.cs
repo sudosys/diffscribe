e4fd84f57762cafc4ffd19e76d1b34946d3e96bf
sudosys <ergin.ahmedtaha@gmail.com>
2025-05-01 16:58:43 +0300

    refactor(dispatcher): improve error handling

--- DiffScribe/CommandDispatcher.cs
+++ DiffScribe/CommandDispatcher.cs
@@ -6,16 +6,25 @@ public class CommandDispatcher(ArgumentValidator argumentValidator, CommandMatch
 {
     public void Dispatch(CommandInfo commandInfo)
     {
-        if (commandMatcher.TryMatch(commandInfo.Name, out var command) && command != null)
-        {
-            if (argumentValidator.Validate(command.DefinedArguments, commandInfo.Arguments))
-            {
-                command.Execute(commandInfo.Arguments);
-            }
-        }
-        else
+        if (!commandMatcher.TryMatch(commandInfo.Name, out var command) || command == null)
         {
             ConsoleWrapper.Error($"Unknown command: {commandInfo.Name}");
+            return;
+        }
+
+        if (!argumentValidator.Validate(command.DefinedArguments, commandInfo.Arguments))
+        {
+            return;
+        }
+        
+        try
+        {
+            command.Execute(commandInfo.Arguments);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine();
+            ConsoleWrapper.Error($"{e.GetType()}: {e.Message}");
         }
     }
 }
