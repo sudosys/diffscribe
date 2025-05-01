037d12c46fe42dc8a43c3c1600d0b5223e6f6205
sudosys <ergin.ahmedtaha@gmail.com>
2025-05-01 16:45:32 +0300

    feat(openai client): update OpenAiClient to use ApiKeyCredential and add NetworkTimeout

--- DiffScribe/AI/OpenAiClient.cs
+++ DiffScribe/AI/OpenAiClient.cs
@@ -1,5 +1,7 @@
+using System.ClientModel;
 using DiffScribe.Configuration;
 using DiffScribe.Extensions;
+using OpenAI;
 using OpenAI.Chat;
 
 namespace DiffScribe.AI;
@@ -19,16 +21,20 @@ public class OpenAiClient(ConfigHandler configHandler)
 
         return true;
     }
-    
-    private ChatClient GetClient(string? apiKey = null) => 
-        new(model: configHandler.Configuration.Llm.ParseApiName(),
-            apiKey: apiKey ?? configHandler.ReadApiKey());
 
     public string SendMessage(params ChatMessage[] message)
     {
         var client = GetClient();
         return SendMessage(client, message);
     }
+
+    private ChatClient GetClient(string? apiKey = null) => 
+        new(model: configHandler.Configuration.Llm.ParseApiName(),
+            credential: new ApiKeyCredential(apiKey ?? configHandler.ReadApiKey()),
+            options: new OpenAIClientOptions
+            {
+                NetworkTimeout = TimeSpan.FromSeconds(30)
+            });
     
     private string SendMessage(ChatClient client, params ChatMessage[] message)
     {
