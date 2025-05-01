bfc76499aab29a39db14a2d61f1e840b4e1040ea
sudosys <ergin.ahmedtaha@gmail.com>
2025-05-01 23:36:18 +0300

    feat: add AppInfo static class with name, version, and owner username

---
+++ DiffScribe/AppInfo.cs
@@ -0,0 +1,8 @@
+namespace DiffScribe;
+
+public static class AppInfo
+{
+    public const string Name = "DiffScribe";
+    public const string Version = "1.0.0";
+    public const string OwnerUsername = "sudosys";
+}
