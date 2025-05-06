66ed710e95443b74f1e43b8598219a7bce34d633
sudosys <ergin.ahmedtaha@gmail.com>
2025-05-07 01:42:03 +0300

    fix(uninstall.ps1): wait for dsc.exe to exit before starting uninstallation

--- DiffScribe/Scripts/uninstall.ps1
+++ DiffScribe/Scripts/uninstall.ps1
@@ -1,4 +1,11 @@
 $dest = "$env:ProgramFiles\DiffScribe"
+$exe = Join-Path $dest "dsc.exe"
+
+$proc = Get-Process | Where-Object { $_.Path -eq $exe }
+if ($proc) {
+    Write-Output "Waiting for dsc.exe to exit..."
+    $proc.WaitForExit()
+}
 
 if (Test-Path $dest) {
     Remove-Item -Recurse -Force $dest
